0be8e636c0eb252c5b0b8f5ca486f30f86d67f6b
kapnuu <pryanichnikov.av@gmail.com>
2016-12-02 21:00:11 +0300

    generate new authorization data in development mode

--- vetka/views.py
+++ vetka/views.py
@@ -3,8 +3,9 @@ from flask import render_template, redirect, url_for, flash, g, send_from_direct
 from vetka import app, models, db, tagcloud, security
 from sqlalchemy import and_, or_, desc
 import random
+import string
 import jinja2
-from os import path, environ
+from os import path, environ, urandom
 
 g_tags = None
 
@@ -87,15 +88,28 @@ def home():
 
 @app.route('/login', methods=['POST'])
 def login():
-    salt = '123456'
+    username = request.form['username']
     password = request.form['password']
-    if password is not None:
-        passw_hash = security.encrypt(password, salt)
-        if passw_hash == '4fc728b424b618c856ddac8f3854bd048247c06f92b851a3c728a83ca2afba6c' and \
-                        request.form['username'] == 'vetka':
+    if username is not None and password is not None:
+        if app.config.get('DEVELOPMENT'):
+            salt = ''.join(random.sample(string.ascii_letters + string.digits, 16))
+            password_hash = security.encrypt(password, salt)
             session['logged_in'] = True
+            print('username: %s salt: %s hash: %s' % (username, salt, password_hash))
         else:
-            flash('Invalid login or password!', category='error')
+            salt = app.config.get('ADMIN_SALT')
+            true_username = app.config.get('ADMIN_USERNAME')
+            true_password = app.config.get('ADMIN_PASSWORD')
+            if salt is not None and true_username is not None and true_password is not None:
+                password_hash = security.encrypt(password, salt)
+                if password_hash == true_password and username == true_username:
+                    session['logged_in'] = True
+                else:
+                    flash('Invalid login or password!', category='error')
+            else:
+                flash('Nobody can do this. Nobody.', category='error')
+    else:
+        flash('Invalid login or password!', category='error')
     return redirect(url_for('home'))
 
 
@@ -179,16 +193,15 @@ def restore(good_id):
         flash('Good <strong>' + good_id + '</strong> not found.', category='error')
         return redirect(url_for('home'))
     if not gg.deleted:
-        flash('Good <strong>' + good_id + '</strong> not deleted. Nothing to restore.', category='error')
+        flash('Good <a href=' + url_for('good', good_id=good_id) + '>' + good_id +
+              '</a> not deleted. Nothing to restore.', category='error')
         return redirect(url_for('home'))
 
     global g_tags
     g_tags = None
 
     gg.deleted = False
-    # db.session.add(gg)
     db.session.commit()
-    # db.session.expunge(gg)
     flash('Good <a href=' + url_for('good', good_id=good_id) + '>' + good_id + '</a> restored. <a href=' +
           url_for('delete', good_id=good_id) + '>Delete?</a>', category='success')
 
@@ -205,16 +218,15 @@ def delete(good_id):
         flash('Good <strong>' + good_id + '</strong> not found.', category='error')
         return redirect(url_for('home'))
     if gg.deleted:
-        flash('Good <strong>' + good_id + '</strong> already deleted.', category='error')
+        flash('Good <a href=' + url_for('good', good_id=good_id) + '>' + good_id + '</a> already deleted.',
+              category='error')
         return redirect(url_for('home'))
 
     global g_tags
     g_tags = None
 
     gg.deleted = True
-    # db.session.add(gg)
     db.session.commit()
-    # db.session.expunge(gg)
     flash('Good <a href=' + url_for('good', good_id=good_id) + '>' + good_id + '</a> deleted. <a href=' +
           url_for('restore', good_id=good_id) + '>Restore?</a>', category='success')
     return redirect(url_for('home'))
