d1d2f8cda23c8d69d2d88bf988c3b15ee5e6f99e
kapnuu <pryanichnikov.av@gmail.com>
2016-12-03 21:52:12 +0300

    fix upgrade

--- db_upgrade.py
+++ db_upgrade.py
@@ -1,5 +1,7 @@
 #!flask/bin/python
 from migrate.versioning import api
 from config import Config
+print('SQLALCHEMY_DATABASE_URI: % s' % Config.SQLALCHEMY_DATABASE_URI)
+print('SQLALCHEMY_MIGRATE_REPO: % s' % Config.SQLALCHEMY_MIGRATE_REPO)
 api.upgrade(Config.SQLALCHEMY_DATABASE_URI, Config.SQLALCHEMY_MIGRATE_REPO)
 print('Current DB version: ' + str(api.db_version(Config.SQLALCHEMY_DATABASE_URI, Config.SQLALCHEMY_MIGRATE_REPO)))
