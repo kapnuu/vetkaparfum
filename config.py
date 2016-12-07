1844bb4d9cdf77eb0afe6e1c2b16f62209ed28af
kapnuu <pryanichnikov.av@gmail.com>
2016-12-07 22:57:08 +0300

    add feedback

--- config.py
+++ config.py
@@ -22,6 +22,15 @@ class Config:
     ADMIN_PASSWORD = os.environ.get('ADMIN_PASSWORD')
     ADMIN_SALT = os.environ.get('ADMIN_SALT')
 
+    SENDGRID_API_TOKEN = os.environ.get('SENDGRID_API_TOKEN')
+    FEEDBACK_RECEIVER = os.environ.get('FEEDBACK_RECEIVER')
+    FEEDBACK_SENDER = os.environ.get('FEEDBACK_SENDER')
+
+    TWILIO_ACCOUNT_SID = os.environ.get('TWILIO_ACCOUNT_SID')
+    TWILIO_AUTH_TOKEN = os.environ.get('TWILIO_AUTH_TOKEN')
+    FEEDBACK_PHONE_SRC = os.environ.get('FEEDBACK_PHONE_SRC')
+    FEEDBACK_PHONE_DST = os.environ.get('FEEDBACK_PHONE_DST')
+
 
 class ProductionConfig(Config):
     DEBUG = False
