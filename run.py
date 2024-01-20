ea69ccd1f249a2450c9b79d8856b6e1a54573bad
kapnuu <pryanichnikov.av@gmail.com>
2024-01-20 18:55:10 +0100

    move to poetry & waitress

--- run.py
+++ run.py
@@ -1,8 +1,13 @@
 import os
+
+from waitress import serve
+
 from vetka import app
 
 app.secret_key = os.urandom(16)
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(debug=app.config['DEBUG'], host='0.0.0.0', port=port)
+    HOST = '127.0.0.1'
+    PORT = int(os.getenv('HTTP_PORT') or 55010)
+    # app.run(host=HOST, port=PORT, debug=True, ssl_context=None)
+    serve(app, host=HOST, port=PORT)
